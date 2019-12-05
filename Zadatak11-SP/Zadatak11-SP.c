5598b06a1502d9160d3e98e0d64f95f6b6e01b8b
100fesb <56272116+100fesb@users.noreply.github.com>
2019-12-05 21:43:52 +0100

    Add files via upload

---
+++ Zadatak11-SP/Zadatak11-SP.c
@@ -0,0 +1,254 @@
+/*
+11. Napisati kod koji za zadane podatke studenata (matièni broj, ime i prezime) pravi
+hash tablicu sa zasebnim redovima. Tablica ima 11 mjesta, a funkcija za preslikavanje
+kljuè raèuna da se zbraja ASCII vrijednost prvih pet slova prezimena i zatim raèuna
+ostatak cjelobrojnog dijeljenja te vrijednosti s velièinom tablice. Studenti s istim
+kljuèem se pohranjuju u vezanu listu sortiranu po prezimenima i imenima.
+Kada se tablica izgradi treba ispisati cijelu tablicu (naglasiti na kojem kljuèu se nalaze
+koji podaci) te ponuditi moguænost da se za odreðenog studenta (prezime i ime) ispiše
+njegov matièni broj.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+
+
+#define BUFFER_LENGTH 1024
+#define N_LENGTH 1024
+#define ERROR -1
+#define SUCCESS 0
+
+typedef struct Student* Pozicija;
+typedef struct Student* Lista;
+typedef struct hashTablica* hashTab;
+
+struct Student {
+	int maticniBroj;
+	char ime[N_LENGTH];
+	char prezime[N_LENGTH];
+	Pozicija Next;
+};
+
+struct hashTablica {
+	int velicinaTablice;
+	Lista* hashLista;		//pokazivac na pokazivac jer je HashTablica polje pokazivaca na strukture tipa Student
+};		
+
+int CitajIzDat(Pozicija);
+Pozicija StvoriNovogS();
+int IzracunajHashKljuc(char*, int);
+hashTab InicijalizacijaTablice(int);
+int UpisuHashTablicu(Pozicija, hashTab);
+int IspisHashTablice(hashTab);
+int IspisPojedinacnogStudenta(hashTab);
+Pozicija TraziStudentaPoMatBr(hashTab, int);
+
+int main()
+{
+	struct Student HeadS;
+	hashTab HeadT;
+	int HashZbroj = 0;
+	int velicinaTablice = 11;
+
+	HeadS.Next = NULL;	// head liste studenti,lista koja se cita iz file-a
+
+	if (CitajIzDat(&HeadS) == -1) {			// ako nije nadjena datoteka da ne nastavlja sa programom
+		system("pause");
+		return ERROR;
+	}
+
+	HeadT = InicijalizacijaTablice(velicinaTablice);	// HeadT pokazuje na 0-ti element polja pokazivaca na liste studenti
+	UpisuHashTablicu(HeadS.Next, HeadT);	// upisuje sve elemente koristeci listu stvorenu u funkciji CitajIzDat - upisuje u odgovarajuce elemente hash tablice
+	IspisHashTablice(HeadT);
+	IspisPojedinacnogStudenta(HeadT);
+
+	system("pause");
+	return 0;
+}
+
+Pozicija StvoriNovogS()
+{
+	Pozicija P = NULL;
+	P = (Pozicija)malloc(sizeof(struct Student));
+	return P;
+}
+
+hashTab InicijalizacijaTablice(int velicinaTablice)
+{
+	hashTab H = NULL;	// stvaramo head element za strukturu tipa hashTablica, zato dolje ide malloc sa sizeof te strukture(hashTablica) -> (a ne velicina tablice)
+	int i = 0;
+
+	H = (hashTab)malloc(sizeof(struct hashTablica));
+	if (!H) return ERROR;
+
+	H->velicinaTablice = velicinaTablice;
+
+	H->hashLista = (Lista*)malloc(sizeof(Lista) * velicinaTablice);		// alociramo prostor za cijelu tablicu (nama 11 mjesta)
+	if (!H->hashLista) return ERROR;
+
+	for (i = 0; i < velicinaTablice; i++)
+	{
+		H->hashLista[i] = NULL;
+	}
+
+	return H;
+}
+
+
+int IzracunajHashKljuc(char* rijec, int velicinaTab)
+{
+	int hashZbroj = 0;
+	int i = 0;
+
+	// algoritam za kljuc 
+	// vrlo jednostavan - zbroji ASCII vrijednosti prvih 5 znakova (zadano je 5) prezimena datog studenta i sa return vraca zadnju znamenku zbroja (modulo 10) to nam je kljuc za 1 element
+	for (i = 0; i < 5; i++)
+		hashZbroj += rijec[i];
+
+	return hashZbroj % velicinaTab;
+}
+
+
+int CitajIzDat(Pozicija P) {
+
+	Pozicija q = NULL, temp = P;	// temp mi sluzi da izbjegnem jednu while petlju -> da zapamti zadnji element i doda na njega - pri tome ne dira Head	
+	char* fileName = NULL;
+	FILE* fp = NULL;
+
+	fileName = (char*)malloc(sizeof(char) * BUFFER_LENGTH);
+
+	if (!fileName) {
+		printf("Greska prilikom alokacije!\n");
+		return ERROR;
+	}
+
+	printf("\nUnesite datoteku za citanje: ");
+	scanf(" %s", fileName);
+
+	fp = fopen(fileName, "r");
+	if (!fp) {
+		printf("\nDatoteka nije otvorena!\n");
+		return ERROR;
+	}
+	
+	while (!feof(fp)) {		// kao i ranije, stvaramo vezanu listu svih studenata, generiranu iz datoteke
+		q = StvoriNovogS();
+		fscanf(fp, " %d %s %s", &q->maticniBroj, q->ime, q->prezime);
+		q->Next = temp->Next;
+		temp->Next = q;
+
+		temp = temp->Next;
+
+	}
+	fclose(fp);
+
+	return 0;
+}
+
+int UpisuHashTablicu(Pozicija P, hashTab H)
+{
+	int i = 0, kljuc = 0;
+	int velTab = H->velicinaTablice;
+	Pozicija temp = NULL;	 // stvaramo temp pokazivac jer triba listu stvorenu iz citanja file-a izbrisat, pa da ne minjamo pokazivace originala
+
+	while (P != NULL) {
+
+		kljuc = IzracunajHashKljuc(P->prezime, velTab);
+		
+		if (H->hashLista[kljuc] != NULL)		// ako vec postoji student s istim kljucem - radi novu vezanu listu
+			H->hashLista[kljuc]->Next = P;
+		else
+			H->hashLista[kljuc] = P;
+
+		temp = StvoriNovogS();		//temp objasnjen gore
+		temp = P;
+		P = P->Next;
+		temp->Next = NULL;
+	}
+}
+
+int IspisHashTablice(hashTab H)
+{
+	int i = 0;
+	int firstInstance = 0;
+	int velTab = H->velicinaTablice;
+	Pozicija P = NULL;
+	P = StvoriNovogS();
+
+	if (!H) return ERROR;
+
+	printf("\n\n\t%10s %10s %10s %10s\n", "Br. kljuca", "Maticni br.", "Prezime", "Ime");
+
+	for (i = 0; i < velTab; i++)
+	{
+		if (H->hashLista[i] == NULL) continue;	// ako nema studenta za pojedini kljuc, vrti opet petlju za iduci kljuc
+		else {
+			P = H->hashLista[i];
+			while (P != NULL) {
+				printf("\t%10d %10d %10s %10s\n", i, P->maticniBroj, P->prezime, P->ime);
+				P = P->Next;
+			}
+		}
+	}
+
+	printf("\n\n");
+
+	return SUCCESS;
+}
+
+int IspisPojedinacnogStudenta(hashTab H)
+{
+	char unos[64];
+	int unosINT = 0;
+	Pozicija P = StvoriNovogS();
+
+	while (1) {
+		printf("Unesite maticni broj trazenog studenta ili 0 za izlaz: ");
+		scanf("%s", unos);
+
+		if (isdigit(unos[0]) == 0) continue; // provjera ako nije unesen broj - ponavljanje petlje
+
+		unosINT = atoi(unos);
+		if (unosINT == 0) break; // izlazak iz petlje kad korinik odabere 0
+
+		P = TraziStudentaPoMatBr(H, unosINT);
+
+		if (!P) {
+			printf("Nismo nasli studenta s tim maticnim brojem!\n");
+			continue;
+		}
+		printf("Trazeni student s maticnim brojem [%s] je: %s %s\n\n", unos, P->prezime, P->ime);
+	}
+	printf("\n\n");
+}
+
+Pozicija TraziStudentaPoMatBr(hashTab H, int unos)
+{
+	int velTab = H->velicinaTablice;
+	int i = 0;
+	Pozicija P = NULL;
+
+	P = StvoriNovogS();
+
+	for (i = 0; i < velTab; i++)
+	{
+		if (H->hashLista[i] == NULL) continue;
+		else {
+			P = H->hashLista[i];
+			while (P != NULL) {
+				if (P->maticniBroj == unos) {
+					return P;
+				}
+				P = P->Next;
+			}
+		}
+	}
+
+	return NULL;
+}
