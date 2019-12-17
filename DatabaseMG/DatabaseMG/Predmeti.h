51950b634e49775d716f6e842e043d9aa2657a3c
Goran Trajanovic <goran.trajanovic.00@fesb.hr>
2019-12-17 12:05:30 +0100

    Izbrisa san duplikat

--- DatabaseMG/DatabaseMG/Predmeti.h
+++
@@ -1,7 +0,0 @@
-//int BrisiPredmet();
-//int BrisiSvePredmete();
-//int PromjeniImePredmeta();
-//int DodajPredmet();
-//int DodajProfesoraPredmetu();
-//int IspisiSvePredmete();
-//int IspisiTrazeniPredmet();
